d4ceabfd6f8f1a848d7c04cd2f40a2bd7f9203eb
Erick <ericksuzart@gmail.com>
2025-04-22 23:01:42 -0300

    Migrate launch file & add bridge communication to ROS

--- launch/desafio.launch.py
+++ launch/desafio.launch.py
@@ -1,46 +1,84 @@
+import os
+
+from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument
-from launch.actions import IncludeLaunchDescription
-from launch.actions import OpaqueFunction
+from launch.actions import (
+    AppendEnvironmentVariable,
+    DeclareLaunchArgument,
+    ExecuteProcess,
+    IncludeLaunchDescription,
+    OpaqueFunction,
+)
 from launch.launch_description_sources import PythonLaunchDescriptionSource
-from launch.substitutions import LaunchConfiguration
-from launch.substitutions import PathJoinSubstitution
-from launch_ros.substitutions import FindPackageShare
+from launch.substitutions import Command, LaunchConfiguration, PathJoinSubstitution
+from launch_ros.actions import Node
+from ros_gz_bridge.actions import RosGzBridge
+from ros_gz_sim.actions import GzServer
 
 
 def generate_launch_description():
-    maze_models = ['model-1', 'model-2', 'model-3']
-
-    x_argument_action = DeclareLaunchArgument(
-        name='x_pose',
-        default_value='0.0',
-        description='Initial x position of the TurtleBot3',
-    )
-
-    y_argument_action = DeclareLaunchArgument(
-        name='y_pose',
-        default_value='-11.0',
-        description='Initial y position of the TurtleBot3',
-    )
-
-    maze_argument_action = DeclareLaunchArgument(
-        name='maze_model',
-        default_value='model-1',
-        choices=maze_models,
-        description='Choose the maze model to spawn',
-    )
-
-    desafio_oxebots_dir = FindPackageShare('desafio_oxebots')
-    pkg_gazebo_ros = FindPackageShare('gazebo_ros')
-
-    use_sim_time = LaunchConfiguration('use_sim_time', default='true')
     x_pose = LaunchConfiguration('x_pose')
     y_pose = LaunchConfiguration('y_pose')
-    maze = LaunchConfiguration('maze_model')
+
+    des_pkg_share = get_package_share_directory('desafio_oxebots')
+    ros_gz_sim_share = get_package_share_directory('ros_gz_sim')
+
+    gz_spawn_model_launch_source = os.path.join(
+        ros_gz_sim_share, 'launch', 'gz_spawn_model.launch.py'
+    )
+
+    tb3_model_path = os.path.join(des_pkg_share, 'models', 'turtlebot3_burger', 'model.sdf')
+    world_path = os.path.join(des_pkg_share, 'worlds', 'desafio_world.sdf')
+    bridge_config_path = os.path.join(des_pkg_share, 'config', 'bridge_config.yaml')
+
+    robot_state_publisher_node = Node(
+        package='robot_state_publisher',
+        executable='robot_state_publisher',
+        parameters=[
+            {'robot_description': Command(['xacro ', LaunchConfiguration('model')])},
+            {'use_sim_time': LaunchConfiguration('use_sim_time')},
+        ],
+    )
+
+    gz_resource_path = AppendEnvironmentVariable(
+        'GZ_SIM_RESOURCE_PATH',
+        os.path.join(des_pkg_share, 'models'),
+    )
+
+    gz_server = GzServer(
+        world_sdf_file=world_path,
+        container_name='ros_gz_container',
+        create_own_container='True',
+        use_composition='True',
+    )
+
+    ros_gz_bridge = RosGzBridge(
+        bridge_name='ros_gz_bridge',
+        config_file=bridge_config_path,
+        container_name='ros_gz_container',
+        create_own_container='False',
+        use_composition='True',
+    )
+
+    spawn_entity = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(gz_spawn_model_launch_source),
+        launch_arguments={
+            'world': 'desafio_world',
+            'topic': '/robot_description',
+            'entity_name': 'turtlebot3_burger',
+            'x': x_pose,
+            'y': y_pose,
+            'z': '0.2',
+        }.items(),
+    )
+
+    maze_models = ['model-1', 'model-2', 'model-3']
+
+    maze_param = LaunchConfiguration('maze_model')
 
     maze_model = PathJoinSubstitution(
         [
-            desafio_oxebots_dir,
+            des_pkg_share,
             'models',
             'maze',
             'model.sdf.temp',
@@ -50,18 +88,11 @@ def generate_launch_description():
     def modify_sdf(context):
         """Modify the maze model SDF file based on the selected maze version."""
         maze_model_path = maze_model.perform(context)
-
-        # Read the template SDF content
         with open(maze_model_path, 'r') as file:
             sdf_content = file.read()
-
-        # Get the selected maze version
-        maze_version = maze.perform(context)
-
+        maze_version = maze_param.perform(context)
         # Replace the placeholder with the actual maze version
         sdf_content = sdf_content.replace('$[version]', f'{maze_version}.dae')
-
-        # Write the modified SDF content to a new file
         maze_model_modified_path = maze_model_path.replace('.temp', '')
         with open(maze_model_modified_path, 'w') as file:
             file.write(sdf_content)
@@ -70,52 +101,40 @@ def generate_launch_description():
 
     modify_sdf_action = OpaqueFunction(function=modify_sdf)
 
-    world = PathJoinSubstitution(
+    return LaunchDescription(
         [
-            desafio_oxebots_dir,
-            'worlds',
-            'desafio_world.world',
+            DeclareLaunchArgument(
+                name='model',
+                default_value=tb3_model_path,
+                description='Absolute path to robot model file',
+            ),
+            DeclareLaunchArgument(
+                name='use_sim_time',
+                default_value='True',
+                description='Flag to enable use_sim_time',
+            ),
+            DeclareLaunchArgument(
+                name='x_pose',
+                default_value='0.0',
+                description='Initial x position of the TurtleBot3',
+            ),
+            DeclareLaunchArgument(
+                name='y_pose',
+                default_value='-11.0',
+                description='Initial y position of the TurtleBot3',
+            ),
+            DeclareLaunchArgument(
+                name='maze_model',
+                default_value='model-1',
+                choices=maze_models,
+                description='Choose the maze model to spawn',
+            ),
+            modify_sdf_action,
+            gz_resource_path,
+            robot_state_publisher_node,
+            gz_server,
+            ExecuteProcess(cmd=['gz', 'sim', '-g'], output='screen'),
+            ros_gz_bridge,
+            spawn_entity,
         ]
     )
-
-    gzserver_cmd = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource(
-            PathJoinSubstitution([pkg_gazebo_ros, 'launch', 'gzserver.launch.py'])
-        ),
-        launch_arguments={'world': world}.items(),
-    )
-
-    gzclient_cmd = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource(
-            PathJoinSubstitution([pkg_gazebo_ros, 'launch', 'gzclient.launch.py'])
-        )
-    )
-
-    robot_state_publisher_cmd = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource(
-            PathJoinSubstitution(
-                [desafio_oxebots_dir, 'launch', 'robot_state_publisher.launch.py']
-            )
-        ),
-        launch_arguments={'use_sim_time': use_sim_time}.items(),
-    )
-
-    spawn_turtlebot_cmd = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource(
-            PathJoinSubstitution([desafio_oxebots_dir, 'launch', 'spawn_turtlebot3.launch.py'])
-        ),
-        launch_arguments={'x_pose': x_pose, 'y_pose': y_pose}.items(),
-    )
-
-    ld = LaunchDescription()
-
-    ld.add_action(x_argument_action)
-    ld.add_action(y_argument_action)
-    ld.add_action(maze_argument_action)
-    ld.add_action(modify_sdf_action)
-    ld.add_action(gzserver_cmd)
-    ld.add_action(gzclient_cmd)
-    ld.add_action(robot_state_publisher_cmd)
-    ld.add_action(spawn_turtlebot_cmd)
-
-    return ld
